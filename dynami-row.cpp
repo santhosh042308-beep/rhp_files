bc1f3eff5c0c5279f5342a2d39dcb65ce3a28e6d
santhosh042308-beep <santhosh042308@gmail.com>
2026-09-14 19:19:09 +0530

    Update dynami-row.cpp

--- dynami-row.cpp
+++ dynami-row.cpp
@@ -1,19 +1,57 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-void combi(string s){
-    int l=s.length();
-    for(int ctr=1;ctr<(1<<l);ctr++){
-        string comb="";
-        for(int ch=0;ch<l;ch++){
-            if((ctr & (1<<ch))>0)
-                comb+=s[ch];
+
+void solve(){
+    int R, C; cin >> R >> C;
+
+    vector<vector<long long>> grid(R, vector<long long>(C));
+    vector<vector<long long>> dp(R, vector<long long>(C));
+
+    for (int row = 0; row < R; row++) {
+        for (int col = 0; col < C; col++) {
+            cin >> grid[row][col];
+            if (col == 0) {
+                dp[row][col] = grid[row][col];
+            }
         }
-        cout<<comb<<endl;
     }
+
+    for (int col = 1; col < C; col++) {
+        long long fmax = -1, smax = -1;
+        int fmax_row = -1;
+
+        for (int row = 0; row < R; row++) {
+            long long val = dp[row][col - 1];
+            if (val > fmax) {
+                smax = fmax;
+                fmax = val;
+                fmax_row = row;
+            } else if (val > smax) {
+                smax = val;
+            }
+        }
+
+        for (int row = 0; row < R; row++) {
+            if (row == fmax_row) {
+                dp[row][col] = grid[row][col] + smax;
+            } else {
+                dp[row][col] = grid[row][col] + fmax;
+            }
+        }
+    }
+
+    long long ans = 0;
+    for (int row = 0; row < R; row++) {
+        ans = max(ans, dp[row][C - 1]);
+    }
+
+    cout << ans << "\n";
 }
-int main(){
-    string s;
-    cin>>s;
-    combi(s);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    while (tc--) solve(); 
     return 0;
 }
