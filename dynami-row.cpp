0f715e26049dc3af47c1f85d38088c3de777ad64
santhosh042308-beep <santhosh042308@gmail.com>
2026-09-14 19:27:01 +0530

    Update dynami-row.cpp

--- dynami-row.cpp
+++ dynami-row.cpp
@@ -1,30 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-vector<long long> getFSMax(const vector<vector<long long>>& dp, int row, int C) {
-    if (C == 1) {
-        return {dp[row][0], 0};
-    }
-    long long fmax = max(dp[row][0], dp[row][1]);
-    long long smax = min(dp[row][0], dp[row][1]);
-    for (int col = 2; col < C; col++) {
-        if (dp[row][col] > fmax) {
-            smax = fmax;
-            fmax = dp[row][col];
-        } else if (dp[row][col] > smax) {
-            smax = dp[row][col];
-        }
-    }
-    return {fmax, smax};
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+void solve(){
     int R, C;
     if (!(cin >> R >> C)) return 0;
 
@@ -41,14 +19,49 @@ int main() {
     }
 
     for (int row = 1; row < R; row++) {
-        vector<long long> fsmax = getFSMax(dp, row - 1, C);
+        long long fmax = dp[row - 1][0];
+        long long smax = 0;
+        
+        if (C > 1) {
+            fmax = max(dp[row - 1][0], dp[row - 1][1]);
+            smax = min(dp[row - 1][0], dp[row - 1][1]);
+            for (int col = 2; col < C; col++) {
+                if (dp[row - 1][col] > fmax) {
+                    smax = fmax;
+                    fmax = dp[row - 1][col];
+                } else if (dp[row - 1][col] > smax) {
+                    smax = dp[row - 1][col];
+                }
+            }
+        }
+
         for (int col = 0; col < C; col++) {
-            dp[row][col] = grid[row][col] + (dp[row - 1][col] == fsmax[0] ? fsmax[1] : fsmax[0]);
+            dp[row][col] = grid[row][col] + (dp[row - 1][col] == fmax ? smax : fmax);
         }
     }
 
-    vector<long long> final_max = getFSMax(dp, R - 1, C);
-    cout << final_max[0] << "\n";
+    long long final_max = dp[R - 1][0];
+    if (C > 1) {
+        long long final_smax = min(dp[R - 1][0], dp[R - 1][1]);
+        final_max = max(dp[R - 1][0], dp[R - 1][1]);
+        for (int col = 2; col < C; col++) {
+            if (dp[R - 1][col] > final_max) {
+                final_smax = final_max;
+                final_max = dp[R - 1][col];
+            } else if (dp[R - 1][col] > final_smax) {
+                final_smax = dp[R - 1][col];
+            }
+        }
+    }
 
+    cout << final_max << "\n";
+
+    return 0;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    while(tc--) solve();
     return 0;
 }
